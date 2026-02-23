4f21fa7256951ad63d459aad222fb24c3734ff66
omar0141 <75172714+omar0141@users.noreply.github.com>
2026-02-23 10:33:26 +0000

    Deploying to main from @ omar0141/om_data_grid@a99d4fc6b991511acda7a9143692c41c6c8aff50 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3977212062" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1387226253" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
